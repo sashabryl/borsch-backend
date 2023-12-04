51222c5e7fe6273879cf14bf0754f10930e17a7f
Olexander Bryl <edlrian814@gmail.com>
2023-12-04 13:00:12 +0200

    Start app borsch service

--- borch_api/settings.py
+++ borch_api/settings.py
@@ -39,7 +39,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "user"
+    "rest_framework",
+    "borsch_service",
+    "user",
 ]
 
 MIDDLEWARE = [
